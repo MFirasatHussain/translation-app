6a7a9d0a2c6e7eff30f97f477965116d338103e1
Firasat Hussain Mohammed <mfirasathussain@gmail.com>
2025-04-17 02:04:20 -0400

    frontend code

---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
+        <h1 className="text-3xl font-bold">Tailwind is working!</h1>
+        <p className="mt-2 text-lg">Welcome to the translation app 🧠</p>
+      </div>
+    );
+  }
+  
